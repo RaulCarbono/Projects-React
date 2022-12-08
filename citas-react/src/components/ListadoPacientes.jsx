b6091028396307dd53222ef8cd4361e9039e420a
Raul Carbono <johndoe@example.com>
2022-12-08 09:01:34 -0500

    add buttons

--- citas-react/src/components/ListadoPacientes.jsx
+++ citas-react/src/components/ListadoPacientes.jsx
@@ -1,24 +1,44 @@
-import React from 'react'
-import Pacientes from './Pacientes'
- 
-const ListadoPacientes = ({pacientes}) => {
-  
+import React from "react";
+import Pacientes from "./Pacientes";
+
+const ListadoPacientes = ({ pacientes }) => {
   return (
-   <div className='w-1/2 lg:w-3/5 h-screen overflow-y-scroll'>
+    <div className="w-1/2 lg:w-3/5 h-screen overflow-y-scroll">
+      {pacientes && pacientes.length ? (
+        <>
+          <h2 className="font-black text-3xl text-center">
+            Listado pacientes{" "}
+          </h2>
 
-        <h2 className='font-black text-3xl text-center'>Listado pacientes </h2>
+          <p className="text-xl mt-5 mb-10 text-center">
+            Administra tus {""}
+            <span className="text-indigo-600 font-bold">
+              {" "}
+              Pacientes y Citas{" "}
+            </span>
+          </p>
 
-        <p className='text-xl mt-5 mb-10 text-center'>
-            Administra tus {''}
-            <span className='text-indigo-600 font-bold'> Pacientes y Citas </span>
-        </p>
-      
-         {pacientes.map(item => (
-          <Pacientes paciente={item}/>
-         ))} 
-      
-   </div>
-  )
-}
+          {pacientes.map((item) => (
+            <Pacientes paciente={item} key={item.id} />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">
+            No hay pacientes{" "}
+          </h2>
 
-export default ListadoPacientes
+          <p className="text-xl mt-5 mb-10 text-center">
+            Comienza agregando Pacientes {""}
+            <span className="text-indigo-600 font-bold">
+              {" "}
+              y aparereceran en este lugar{" "}
+            </span>
+          </p>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default ListadoPacientes;
--- citas-react/src/components/Pacientes.jsx
+++ citas-react/src/components/Pacientes.jsx
@@ -1,7 +1,6 @@
-const Pacientes = ({paciente}) => {
+const Pacientes = ({ paciente }) => {
+  const { name, propietario, email, fecha, sintomas } = paciente;
 
-  const {name, propietario, email, fecha,sintomas} = paciente
-  
   return (
     <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl ">
       <p className="font-bold mb-3 text-gray-700 uppercase">
@@ -24,6 +23,21 @@ const Pacientes = ({paciente}) => {
         Sintomas : {""}
         <span className="font-normal normal-case">{sintomas} </span>
       </p>
+
+      <div className="flex justify-between mt-10">
+        <button
+          type="button"
+          className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white font-bold uppercase rounded-lg"
+        >
+          Editar
+        </button>
+        <button
+          type="button"
+          className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white font-bold uppercase rounded-lg"
+        >
+          Eliminar
+        </button>
+      </div>
     </div>
   );
 };
