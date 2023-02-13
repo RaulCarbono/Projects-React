aef67f563c06ede3c65ca335a0fbfa9585496130
Raul Carbono <carbonoraul2@gmail.com>
2023-02-13 05:41:43 -0500

    pokePrueba

---
+++ pokemon-Typescript/src/App.tsx
@@ -0,0 +1,10 @@
+import { Dialog } from "./styles/customized/Dialog";
+
+function App() {
+  return <div className="principal">
+    hola
+    <Dialog  />
+  </div>;
+}
+
+export default App;
---
+++ pokemon-Typescript/src/styles/customized/Dialog.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const Dialog = styled.div`
+background: red;
+width:100px
+height:100px
+color:black`
