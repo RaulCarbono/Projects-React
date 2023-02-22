3e4902860a66668eabd8f5055f344ace43833801
RaulCarbono <88171012+RaulCarbono@users.noreply.github.com>
2023-02-22 17:04:15 -0500

    functionality edit

--- tablet/src/components/Sidebar.jsx
+++ tablet/src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ const Sidebar = () => {
   return (
     <>
     <div className="container_principal">
-        <div className="principal_container_sidebar">
+       <div className="principal_container_sidebar">
             <div className="sidebar_container">
               <div className="sidebar_logo">
                 <img className="sidebar_img_logo" src="http://localhost:3000/static/media/logo_lr.8d925b3a.svg"></img>
