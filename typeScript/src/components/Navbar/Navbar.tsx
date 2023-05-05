3185fa2ea264d47dc08b69d6dbff997039798408
RaulCarbono <88171012+RaulCarbono@users.noreply.github.com>
2023-05-05 17:02:13 -0500

    test

--- typeScript/src/components/Navbar/Navbar.tsx
+++ typeScript/src/components/Navbar/Navbar.tsx
@@ -1,15 +1,11 @@
-import {
-	AppBar,
-	Toolbar,
-	Typography
-} from "@mui/material";
+import { AppBar, Toolbar, Typography } from "@mui/material";
 import React from "react";
 
-export type NavbarInterface = {}
+export type NavbarInterface = {};
 
 const Navbar: React.FC<NavbarInterface> = () => {
   return (
-    <AppBar position="static">
+    <AppBar position="fixed">
       <Toolbar>
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Programing React TEST
--- typeScript/src/pages/Home/Home.tsx
+++ typeScript/src/pages/Home/Home.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
-import "../../index.css"
+import React from "react";
+import { DataGrid, GridRenderCellParams } from "@mui/x-data-grid";
+import "../../index.css";
+import { People } from "@/data/people";
 
-export type HomeInterface = {
-
-}
+export type HomeInterface = {};
 
 const Home: React.FC<HomeInterface> = () => {
-	return <div >Home</div>;
+  const pageSize: number = 5;
+  const colums = [
+    {
+      field: "name",
+      headerName: "Name",
+      flex: 1,
+      minWidth: 150,
+      renderCell: (params: GridRenderCellParams) => <>{params.field}</>,
+    },
+  ];
+
+  return (
+    <div>
+      <DataGrid
+        rows={People}
+        columns={colums}
+        disableColumnSelector
+        disableRowSelectionOnClick
+        rowCount={3}
+        rowThreshold={3}
+        pageSizeOptions={[pageSize]}
+      />
+    </div>
+  );
 };
 
 export default Home;
