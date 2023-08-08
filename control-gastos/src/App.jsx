83f0c531c06e51d9dfbd7f8df13558dfd17c086c
RaulCarbono <88171012+RaulCarbono@users.noreply.github.com>
2023-08-08 17:01:13 -0500

    control gastos modal

--- control-gastos/src/App.jsx
+++ control-gastos/src/App.jsx
@@ -38,6 +38,7 @@ function App() {
         <Modal
           setModal={setModal}
           animationModal={animationModal}
+          setAnimarModal={setAnimationModal}
         />
       )}
     </>
--- control-gastos/src/components/Modal.jsx
+++ control-gastos/src/components/Modal.jsx
@@ -1,9 +1,14 @@
 import CloseBtn from '../img/cerrar.svg';
 
-export const Modal = ({ setModal, animationModal }) => {
+export const Modal = ({ setModal, animationModal, setAnimationModal }) => {
   const hideModal = () => {
-    setModal(false);
+    setAnimationModal(false);
   };
+
+  setTimeout(() => {
+    setModal(false);
+  }, 6000);
+
   return (
     <div className="modal">
       <div className="cerrar-modal">
@@ -13,8 +18,44 @@ export const Modal = ({ setModal, animationModal }) => {
           onClick={hideModal}
         />
       </div>
-      <form className={`formulario ${animationModal ? 'animar' : ''}`}>
-        <legend>Nuevo gasto</legend>
+      <form className={`formulario ${animationModal ? 'animar' : 'cerrar'}`}>
+        <legend>Nuevo Gasto</legend>
+        <div className="campo">
+          <label htmlFor="nombre">Nombre Gasto</label>
+
+          <input
+            id="Nombre"
+            type="text"
+            placeholder="Añade el nombre del gasto"
+          />
+        </div>
+        <div className="campo">
+          <label htmlFor="nombre">Cantidad</label>
+
+          <input
+            id="cantidad"
+            type="number"
+            placeholder="Añade la cantidad del gasto"
+          />
+        </div>
+        <div className="campo">
+          <label htmlFor="categoria">Categoria</label>
+
+          <select id="categoria">
+            <option value=""> -- Seleccione --</option>
+            <option value="ahorro"> Ahorro </option>
+            <option value="comida"> Comidad </option>
+            <option value="casa"> Casa </option>
+            <option value="gastos"> Gastos Varios </option>
+            <option value="ocio"> Ocio </option>
+            <option value="salud"> Salud </option>
+            <option value="subscripciones"> Subscripciones </option>
+          </select>
+        </div>
+        <input
+          type="submit"
+          value="Añadir Gasto"
+        />
       </form>
     </div>
   );
